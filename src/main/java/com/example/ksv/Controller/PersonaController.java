218978d54eea1eaaba804541d2037ba8a41c0546
karla <silvargaskarlaa@gmail.com>
2023-04-11 20:57:19 -0300

    modificacion docker y target

--- src/main/java/com/example/ksv/Controller/PersonaController.java
+++ src/main/java/com/example/ksv/Controller/PersonaController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 @RestController
-@CrossOrigin(origins ={"https://kasvfrontend.web.app/","http://localhost:4200"})
+@CrossOrigin(origins ="http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService iPersonaServ;
     
