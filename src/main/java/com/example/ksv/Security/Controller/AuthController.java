86f298c95ea8ea6c09203209c9ffc66bf89e5c3b
karla <silvargaskarlaa@gmail.com>
2023-04-18 23:34:46 -0300

    url de firebase en el back

--- src/main/java/com/example/ksv/Security/Controller/AuthController.java
+++ src/main/java/com/example/ksv/Security/Controller/AuthController.java
@@ -32,7 +32,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin (origins ={"https://kasvfrontend.web.app/","http://localhost:4200"})
+@CrossOrigin (origins ={"https://kasvfrontend.web.app","http://localhost:4200"})
 public class AuthController {
     
      @Autowired
